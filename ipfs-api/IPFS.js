0fbe68a6ed836f7335b8651e82ee6bb6aa325e84
Haardik <hhaardik@edu.uwaterloo.ca>
2019-11-09 05:52:08 -0500

    add cors to IPFS api

--- ipfs-api/IPFS.js
+++ ipfs-api/IPFS.js
@@ -1,21 +1,24 @@
 //Required modules
 const IpfsNode = require('./node.js');
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 const ipfs = new IpfsNode();
 //Addfile router for adding file a local file to the IPFS network without any local node
-app.post("/addfile", async function(req, res) {
-    res.send(await ipfs.addMessage(req.body))
+app.post('/addfile', async function(req, res) {
+  res.send(await ipfs.addMessage(req.body));
 });
 //Getting the uploaded file via hash code.
-app.get("/getfile", async function(req, res) {
-    hashes = req.body.hashes
+app.get('/getfile', async function(req, res) {
+  hashes = req.body.hashes;
 
-    res.send(await ipfs.getMessages(hashes))
+  res.send(await ipfs.getMessages(hashes));
 });
 
-app.listen(3000, () => console.log("App listening on port 3000!"));
+app.listen(3000, () => console.log('App listening on port 3000!'));
