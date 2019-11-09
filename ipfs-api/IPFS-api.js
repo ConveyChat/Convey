72495c3e0db7c957e4eef8445f3fe8149925071d
Haardik <hhaardik@edu.uwaterloo.ca>
2019-11-09 10:04:32 -0500

    delete unused file

--- ipfs-api/IPFS-api.js
+++
@@ -1,6 +0,0 @@
-// endpoint is the URL for the info were requesting
-
-'use strict'
-
-const IPFS = require('ipfs-mini')
-const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
