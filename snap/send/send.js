d27e0806773f26b874a53ab7fc537794416d66e8
Haardik <hhaardik@edu.uwaterloo.ca>
2019-11-09 21:16:30 -0500

    send bundle working ipfs add

---
+++ snap/send/send.js
@@ -0,0 +1,33 @@
+const ethers = require('ethers');
+const provider = ethers.getDefaultProvider('ropsten');
+const axios = require('axios').default;
+
+async function send(ethAddress, ensName, msg) {
+  var addr;
+  const domain = ensName;
+  const message = msg;
+
+  // if its the ens
+  if (domain.includes('.eth')) {
+    provider.resolveName(domain).then(function(address) {
+      console.log('Address: ' + address);
+      addr = address;
+    });
+  } else {
+    addr = domain;
+  }
+
+  const response = await axios.post('http://localhost:3000/addfile', {
+    receiver: addr,
+    sender: ethAddress,
+    body: message
+  });
+  return {
+    hash: response.data,
+    receiver: addr
+  };
+}
+
+module.exports = {
+  send: send
+};
