83ee6275008530cc2484a78d912b4d727ea7f0e4
Haardik <hhaardik@edu.uwaterloo.ca>
2019-11-09 20:34:17 -0500

    clean up extra files

--- snap/send.js
+++ snap/send.js
@@ -1,66 +1,47 @@
-    
-const ethers = require('ethers');
-const createKeccakHash = require('keccak')
-var provider = "window.ethereum";
+var provider = 'window.ethereum';
 
-  async function send(ethAddress, ensName, msg) {
- 
-    var addr;
-    const domain = ensName;
-    const message = msg;
+async function send(ethAddress, ensName, msg) {
+  var addr;
+  const domain = ensName;
+  const message = msg;
 
-    // if its the ens
-    if (domain.includes(".")) {
-      provider.resolveName(domain).then(function(address) {
-        console.log("Address: " + address);
-        addr = address;
-      });
-    } else {
-      addr = domain;
-    }
-
-    // if the address isn't valid
-    // if (!toChecksumAddress(addr)) {
-    //   alert('Address is not valid');
-    //   return;
-    // }
-
-    //create JSON
-    var obj = new Object();
-    obj.receiver = addr;
-    obj.sender = ethAddress;
-    obj.body = message;
-
-    const response = await fetch('http://localhost:3000/addfile', {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      headers: {
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: obj // body data type must match "Content-Type" header
+  // if its the ens
+  if (domain.includes('.')) {
+    provider.resolveName(domain).then(function(address) {
+      console.log('Address: ' + address);
+      addr = address;
     });
-    //const body = await response.json();
-
-    console.log(response);
+  } else {
+    addr = domain;
   }
 
-  async function toChecksumAddress (address) {
-    address = address.toLowerCase().replace('0x', '')
-    var hash = createKeccakHash('keccak256').update(address).digest('hex')
-    var ret = '0x'
+  // if the address isn't valid
+  // if (!toChecksumAddress(addr)) {
+  //   alert('Address is not valid');
+  //   return;
+  // }
 
-    for (var i = 0; i < address.length; i++) {
-      if (parseInt(hash[i], 16) >= 8) {
-        ret += address[i].toUpperCase()
-      } else {
-        ret += address[i]
-      }
-  }
+  //create JSON
+  let obj = {
+    receiver: addr,
+    sender: ethAddress,
+    body: message
+  };
 
-  return ret
+  const response = await fetch('http://localhost:3000/addfile', {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: obj
+  });
+
+  //const body = await response.json();
+
+  console.log(response);
 }
 
-  module.exports = {
-    send: send 
-  };
+module.exports = {
+  send: send
+};
